4484d08139fa2860213fd1703a1141e2b0c1ac76
Lucas Franceschino <lucas@cryspen.com>
2024-12-04 14:09:13 +0100

    misc: nicer dummy comments

--- tests/coverage/example-crate/src/lib.rs
+++ tests/coverage/example-crate/src/lib.rs
@@ -1,17 +1,9 @@
 fn add_or_zero(x: u8, y: u8) -> u8 {
-    // AA
-    // AA
-    // AA
-    // AA
+    // Some irrelevant comment line
     if (x as u16) + (y as u16) < 256 {
         x + y
     } else {
-        // AA
-        // AA
-        // AA
+        // Another comment
         0
-        // AA
-        // AA
-        // AA
     }
 }
